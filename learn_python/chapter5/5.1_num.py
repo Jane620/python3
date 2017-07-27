c9011586e7145e51a2a59f861b37a02ce4de787f
jfwang <jfwang001@mail.nfsq.com.cn>
2017-07-27 08:37:13 +0800

    learn_python5

--- learn_python/chapter5/5.1_num.py
+++ learn_python/chapter5/5.1_num.py
@@ -10,3 +10,18 @@ int_10 = 10
 print(hex(int_10))
 print(oct(int_10))
 print(bin(int_10))
+
+# slice最后一位为步长
+list_test = [x for x in range(10)]
+list_test2 = list_test[1:7:2]
+list_test3 = list_test[slice(1, 7, 2)]
+print(list_test2)
+print(list_test3)
+
+# 多项比较
+x = 1
+y = 2
+z = 3
+print(x < y < z )
+
+print(0.1*3-0.3)
---
+++ learn_python/chapter5/5.2_divide.py
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 -*-
+import math
+
+value1 = math.floor(2.5)
+value2 = math.floor(-2.5)
+value3 = math.trunc(2.5)
+value4= math.trunc(-2.5)
+print("{0},{1},{2},{3}".format(value1,value2,value3,value4))
