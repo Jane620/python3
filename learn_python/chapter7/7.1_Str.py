176451d7c4943efd8e01ec8aaa130cd3d8f83335
jfwang <jfwang001@mail.nfsq.com.cn>
2017-07-31 09:04:21 +0800

    learn_python7

--- learn_python/chapter7/7.1_Str.py
+++ learn_python/chapter7/7.1_Str.py
@@ -1,20 +1,23 @@
 #-*- coding:utf-8 -*-
 
 B = '1101'
+O = '7377'
 I = 0
-while B != '':
+IO = 0
+while O != '':
     # 计算方式为当前值*2+下一位的值
-    I = I*2 + (ord(B[0])-ord('0'))
-    B = B[1:]
+    #I = I*2 + (ord(B[0])-ord('0'))
+    IO = IO * 8 + (ord(O[0])-ord('0'))
+    print(ord(O[0])-ord('0'))
+    #B = B[1:]
+    O = O[1:]
 
-print(I)
+print('二进制:',I)
+print('八进制:',IO)
 #等价于 int 或者  <<
 I2 = int('1101',2)
 print(I2)
+print(int('7377',8))
 
-B2 = list(reversed('1101'))
-I3 = 0
-while B2 != 
 
-print(B2)
 
