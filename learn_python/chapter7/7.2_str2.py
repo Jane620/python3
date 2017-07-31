58299254c811a56b5a06c9ce921058dcca2ca173
jfwang <vans620@163.com>
2017-07-31 22:28:26 +0800

    learn_python7.2

---
+++ learn_python/chapter7/7.2_str2.py
@@ -0,0 +1,14 @@
+#-*- coding:utf-8 -*-
+
+# dict 格式化字符串
+reply  = '''my name is %(name)s, i'm %(age)d years old.'''
+values = {'name':'john','age':4}
+print(reply % values)
+
+# vars()返回包含本函数或文件中定义的变量
+name = 'kate'
+age = 4
+print(vars())
+
+if 'name' in vars():
+    print(True)
