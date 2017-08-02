fc5cf5f1447a76ce57c149cf7c2db66bf1b5a2c9
jfwang <vans620@163.com>
2017-08-02 22:48:04 +0800

    learn_python10

--- learn_python/chapter10/10.1_while.py
+++ learn_python/chapter10/10.1_while.py
@@ -4,5 +4,12 @@ while True:
     reply = input("enter a word:")
     if reply == 'stop':
         break
-    print(reply.upper())
+    try:
+        num = int(reply)
+    except ValueError:
+        print('not a number')
+    # 此处的else同try结合，并非为上面if
+    else:
+        print(int(reply)**2)
 
+print('bye')
