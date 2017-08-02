86223a4a15b8b42de966101fbeec78ceb1b80c6a
jfwang <jfwang001@mail.nfsq.com.cn>
2017-08-02 17:30:25 +0800

    learn_python9

---
+++ learn_python/chapter10/10.1_while.py
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 -*-
+
+while True:
+    reply = input("enter a word:")
+    if reply == 'stop':
+        break
+    print(reply.upper())
+
--- learn_python/chapter9/9.3_pickle.py
+++ learn_python/chapter9/9.3_pickle.py
@@ -15,3 +15,25 @@ dict_file.close()
 read_file = open(r'..\resource\pickle.pkl','rb')
 line = pickle.load(read_file)
 print(line)
+
+#二进制处理文件
+#>i4sh解释
+# > 用于C或者C++中字节对齐
+# i -> integer -> 7
+# 4s -> 4 strings -> spam
+# h -> unsigned short -> 8
+
+bin_file = open(r'..\resource\data.bin','wb')
+import struct
+
+data = struct.pack('>i4sh', 17, b'spam', 8)
+print(data)
+bin_file.write(data)
+bin_file.close()
+
+F = open(r'..\resource\data.bin','rb')
+data_2 = F.read()
+print(data_2)
+values = struct.unpack('>i4sh',data_2)
+F.close()
+print(values)
